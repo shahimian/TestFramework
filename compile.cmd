b9c6ec1fa58cce963524eddc44eafc196c37656b
shahimian <shahimian@gmail.com>
2019-01-13 10:08:23 +0330

    remove main.cpp and main.o

--- compile.cmd
+++ compile.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set TF=TestFramework
-g++ -c Exception.cpp %TF%.cpp main.cpp
+g++ -c Exception.cpp %TF%.cpp
 g++ -shared -o %TF%.dll Exception.o %TF%.o -Wl,--out-implib,TestFramework.a
-g++ -o main.exe Exception.o %TF%.o main.o
+g++ -o main.exe Exception.o %TF%.o
 del *.a *.o
